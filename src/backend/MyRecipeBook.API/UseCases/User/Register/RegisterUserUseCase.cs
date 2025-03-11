2b45b2d7cd75f9573b38259c95db21f8f31a8a6c
Andressa Silva <andressacosta@pecege.com>
2025-03-11 10:19:06 -0300

    FEAT: Creating userCases

---
+++ src/backend/MyRecipeBook.API/UseCases/User/Register/RegisterUserUseCase.cs
@@ -0,0 +1,27 @@
+﻿using MyRecipeBook.Comunication.Requests;
+using MyRecipeBook.Comunication.Responses;
+
+namespace MyRecipeBook.API.UseCases.User.Register;
+
+public class RegisterUserUseCase
+{
+    public ResponseRegisteredUserJson Execute(RequestRegisterUserJson request)
+    {
+        Validate(request);
+        return new ResponseRegisteredUserJson
+        {
+            Name = request.Name,
+        };
+    }
+
+    private void Validate(RequestRegisterUserJson request)
+    {
+        var validator = new RegisterUserValidator();
+        var result = validator.Validate(request);
+        if (!result.IsValid) 
+        {
+            var errorMessage = result.Errors.Select(error => error.ErrorMessage);
+            throw new Exception();
+        }
+    }
+}
