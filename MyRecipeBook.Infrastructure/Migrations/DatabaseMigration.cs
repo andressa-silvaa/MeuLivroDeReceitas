f7970f8b7c7b42fbe23d769b7deff5b61db11eb1
Andressa Silva <andressacosta@pecege.com>
2025-07-20 19:40:38 -0300

    FIX: Adicionando a lógica responsável pela criação do database

---
+++ MyRecipeBook.Infrastructure/Migrations/DatabaseMigration.cs
@@ -0,0 +1,23 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+
+namespace MyRecipeBook.Infrastructure.Migrations;
+public static class DatabaseMigration
+{
+    public static void Migrate(string connection)
+    {
+        var connectionStringBuilder = new SqlConnectionStringBuilder(connection);
+        var databaseName = connectionStringBuilder.InitialCatalog;
+        connectionStringBuilder.Remove("Database");
+
+        using var databaseConnection = new SqlConnection(connectionStringBuilder.ConnectionString);
+        
+        var parameters = new DynamicParameters();
+        parameters.Add("name", databaseName);
+
+        var records = databaseConnection.Query("SELECT * FROM sys.databases WHERE name = @name", parameters);
+
+        if (!records.Any())
+            databaseConnection.Execute($"CREATE DATABASE {databaseName}");
+    }
+}
